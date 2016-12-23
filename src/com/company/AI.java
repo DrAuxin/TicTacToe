6cc0ee061f51b3e9c6482924f6b688c9a2a74b88
Dr. Auxin <paul.torrillo@gmail.com>
2016-12-23 10:30:23 -0500

    hi

---
+++ src/com/company/AI.java
@@ -0,0 +1,22 @@
+package com.company;
+
+/**
+ * Created by pt696 on 12/23/16.
+ */
+public class AI {
+    public static int move(int j[][])
+    {
+        int danger=2;
+        int sum;
+        while (true)
+    {
+        for (int w = 0; w < j.length; w++)
+        {
+
+
+
+
+            
+        }
+    }
+}
