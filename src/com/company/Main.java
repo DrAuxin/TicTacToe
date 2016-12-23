c7a82b829d21398d9d730e9fe19a257144ba0bb9
Dr. Auxin <paul.torrillo@gmail.com>
2016-12-23 10:10:37 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String board [][] = {{".", ".", "."},{ ".", ".", "."},{ ".", ".","."}};
+        boardPrint bk = new boardPrint(board);
+        bk.printBoard();
+
+}
+}
---
+++ src/com/company/boardPrint.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Created by pt696 on 12/16/16.
+ */
+public class boardPrint {
+    public String b[][];
+    public int g;
+    public boardPrint(String a[][])
+    {
+      b=a;
+    }
+    public void printBoard()
+    {
+        g = 0;
+        for (int k = 0; g < 3; k++) {
+            System.out.print(b[k][g] + " ");
+            if (k >= 2) {
+                System.out.println();
+                g++;
+                k = -1;
+            }
+        }
+    }
+}
