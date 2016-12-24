39547b1311d0728a399c533e82c4a35fd7cb6a5c
Paul <paul.torrillo@gmail.com>
2016-12-24 16:09:11 -0500

    hi

--- src/com/company/boardPrint.java
+++ src/com/company/boardPrint.java
@@ -12,11 +12,16 @@ public class boardPrint {
     }
     public void printBoard()
     {
+        System.out.println("  1 2 3");
+        System.out.print("1 ");
         g = 0;
         for (int k = 0; g < 3; k++) {
             System.out.print(b[g][k] + " ");
             if (k >= 2) {
                 System.out.println();
+                if (g+2<4) {
+                    System.out.print(g + 2 + " ");
+                }
                 g++;
                 k = -1;
             }
